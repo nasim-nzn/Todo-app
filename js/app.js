3f7f72fc36f93849b815fbf1f22802cff92e4afd
nasim <nasimniaziann@gmail.com>
2024-02-04 17:57:11 +0330

    Capture todo values and set up event handling

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addButton = document.getElementById("add-button");
+
+const todos = [];
+
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+
+  const todo = {
+    task: task,
+    date: date,
+    completed: false,
+  };
+  if (task) {
+    todos.push(todo);
+    taskInput.value = "";
+    dateInput.value = "";
+    console.log(todos);
+  } else {
+    alert("warning");
+  }
+};
+
+addButton.addEventListener("click", addHandler);
