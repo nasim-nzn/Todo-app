da11843f9109273663217fe4d243607945a45892
nasim <nasimniaziann@gmail.com>
2024-02-06 11:01:48 +0330

    Implement dynamic display of todos

--- js/app.js
+++ js/app.js
@@ -2,9 +2,15 @@ const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
 const alertMessage = document.getElementById("alert-message");
+const todosBody = document.querySelector("tbody");
 
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 console.log(todos);
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 const generateId = () => {
   return Math.round(
@@ -25,8 +31,26 @@ const showAlert = (message, type) => {
   }, 2000);
 };
 
-const saveToLocalStorage = () => {
-  localStorage.setItem("todos", JSON.stringify(todos));
+const displayTodos = () => {
+  todosBody.innerHTML = "";
+  if (!todos.length) {
+    todosBody.innerHTML = "<tr><td colspan='4'>No task found!</td></tr>";
+    return;
+  }
+  todos.forEach((todo) => {
+    todosBody.innerHTML += `
+      <tr>
+          <td>${todo.task}</td>
+          <td>${todo.date || "No Date"}</td>
+          <td>${todo.completed ? "Completed" : "Pending"}</td>
+          <td>
+              <button>Edit</button>
+              <button>Do</button>
+              <button>Delete</button>
+          </td>
+      </tr>
+    `;
+  });
 };
 
 const addHandler = () => {
@@ -42,13 +66,17 @@ const addHandler = () => {
   if (task) {
     todos.push(todo);
     saveToLocalStorage();
+    displayTodos();
     taskInput.value = "";
     dateInput.value = "";
-    // console.log(todos);
-    showAlert("todo added successfully.", "success");
+    showAlert("Todo added successfully.", "success");
   } else {
-    showAlert("please enter a todo!", "error");
+    showAlert("Please enter a todo!", "error");
   }
 };
 
+
+
+window.addEventListener("load", displayTodos);
 addButton.addEventListener("click", addHandler);
+
